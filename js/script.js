45e34493b5520d76e0d8c01a598ed88c8df79dbb
thenerbiumproject <109812990+thenerbiumproject@users.noreply.github.com>
2022-09-27 19:26:18 +0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,9 @@
+(function() {
+    var cx = "54646360ee7b741f5";
+    var gcse = document.createElement("script");
+    gcse.type = "text/javascript";
+    gcse.async = true;
+    gcse.src = "https://cse.google.com/cse.js?cx=" + cx;
+    var s = document.getElementsByTagName("script")[0];
+    s.parentNode.insertBefore(gcse, s);
+  })();
